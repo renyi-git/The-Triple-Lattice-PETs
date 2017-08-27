6380b1d0e225ad7829608a7470e84cdecb8424e8
renyiboston <renyi.ny2009@gmail.com>
2017-08-27 00:53:13 -0400

    triple lattice pets

---
+++ src/triplelatticepets/ProofException.java
@@ -0,0 +1,14 @@
+package triplelatticepets;
+
+
+public class ProofException extends RuntimeException {
+
+  public ProofException() {
+    super();
+  }
+
+  public ProofException(String s) {
+    super(s);
+  }
+
+}
